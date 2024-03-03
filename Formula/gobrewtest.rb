b7f2bfb2651dd3cb68e05f4d2d378dbda80ce9d0
Dhruv Thakur <dhruvt93@gmail.com>
2024-03-03 12:53:45 +0100

    chore: upgrade gobrewtest to 0.1.4

--- Formula/gobrewtest.rb
+++ Formula/gobrewtest.rb
@@ -1,10 +1,11 @@
 class Gobrewtest < Formula
   desc "A completely useless binary; used for testing purposes"
   homepage "https://github.com/dhth/gobrewtest"
-  version "0.1.2"
+  version "0.1.4"
 
   on_macos do
-    url "https://github.com/dhth/gobrewtest/releases/download/v0.1.2/gobrewtest_Darwin_arm64.tar.gz"
+    url "https://github.com/dhth/gobrewtest/releases/download/v0.1.4/gobrewtest_Darwin_arm64.tar.gz"
+    sha256 "0e9993283ef4843935bcc226b8b4af4e1c3637aaa5a09e2859e9a56fcdbd3f9f"
 
     def install
       bin.install "gobrewtest"
